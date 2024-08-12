037d6a5e43b0d83ca05e88cae669d3c053fd464e
Suryakirankadari <93572415+Suryakirankadari@users.noreply.github.com>
2024-08-12 07:58:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,8 +63,8 @@ if ingredients_list:
 
 # API request to Fruityvice
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-  if fruityvice_response.status_code == 200:
-       fruityvice_data = fruityvice_response.json()
-       fv_df = st.dataframe(data=fruitvice_data,use_container_width=True)
-  else:
-      st.error("failed to fetch datam")
+if fruityvice_response.status_code == 200:
+    fruityvice_data = fruityvice_response.json()
+    fv_df = st.dataframe(data=fruitvice_data,use_container_width=True)
+else:
+     st.error("failed to fetch datam")
